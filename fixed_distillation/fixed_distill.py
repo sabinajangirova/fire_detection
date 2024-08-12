c98afec544ecf3a44534b22fdfe4aa3994024af6
Sabina Jangirova <furiyayuhu@gmail.com>
2024-08-12 18:32:25 +0500

    1000 epochs

--- fixed_distillation/fixed_distill.py
+++ fixed_distillation/fixed_distill.py
@@ -134,7 +134,7 @@ class Distiller(nn.Module):
         return {"student_loss": student_loss.item()}
 
 # Setup logging
-log_file = 'distill_vit16_fixed_21.log'
+log_file = 'distill_vit16_fixed_22.log'
 logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s %(message)s')
 
 def log_system_usage():
@@ -221,7 +221,7 @@ distillation_loss_fn = nn.KLDivLoss(reduction='batchmean')
 distiller = Distiller(student=student_model, teacher=model)
 distiller.compile(optimizer, criterion, distillation_loss_fn, alpha=0.1, temperature=7)
 
-num_epochs = 500
+num_epochs = 1000
 train_losses = []
 val_losses = []
 best_val_loss = float('inf')
@@ -258,7 +258,7 @@ for epoch in range(num_epochs):
     # Save the best model
     if epoch_loss < best_val_loss:
         best_val_loss = epoch_loss
-        torch.save(student_model.state_dict(), '/tmp/best_student_vit16_model_fixed_21.pth')
+        torch.save(student_model.state_dict(), '/tmp/best_student_vit16_model_fixed_22.pth')
         best_preds = all_preds
         best_labels = all_labels
     
@@ -276,11 +276,11 @@ plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.legend()
 plt.title('Training and Validation Losses')
-plt.savefig('losses_plot_distilled_fixed_21.png')
+plt.savefig('losses_plot_distilled_fixed_22.png')
 plt.close()
 
 # Reload the best model weights
-student_model.load_state_dict(torch.load('/tmp/best_student_vit16_model_fixed_21.pth'))
+student_model.load_state_dict(torch.load('/tmp/best_student_vit16_model_fixed_22.pth'))
 
 # Generate confusion matrix for the best model
 distiller.eval()
@@ -303,7 +303,7 @@ sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=class_na
 plt.xlabel('Predicted')
 plt.ylabel('True')
 plt.title('Best Model Confusion Matrix')
-plt.savefig('best_model_confusion_matrix_distilled_fixed_21.png')
+plt.savefig('best_model_confusion_matrix_distilled_fixed_22.png')
 plt.close()
 
 # Save the trained student model
